064d70a777c05447a287112044446d7f9e06fd36
Le Anh Duc <=>
2024-04-28 22:18:54 +0700

    folder structure redesign

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/scss/bootstrap.scss';
 // import './assets/scss/bootstrap_custom.scss';
-import Header from "./layouts/header/Header";
+import Header from "./components/Header/Header";
 
 function App() {
     return (
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Nav from "./Navbar";
+import Nav from "../Nav/Navbar";
 import HeaderInfo from "./HeaderInfo";
 
 export default function Header() {
