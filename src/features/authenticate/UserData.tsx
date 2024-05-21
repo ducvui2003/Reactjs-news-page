44e0d1e345e162db7e0b79f20e6e5c496df3b212
Giang Nguyen <giangtruongnguyen02082003@gmail.com>
2024-05-21 16:42:27 +0700

    upload

---
+++ src/features/authenticate/UserData.tsx
@@ -0,0 +1,8 @@
+import {User} from "../types/user.type";
+
+export const userData: User [] = [
+    {
+        email: "ducvui2003@gmail.com",
+        password: "123123",
+    }
+]
---
+++ src/features/store.ts
@@ -0,0 +1,14 @@
+import {configureStore} from '@reduxjs/toolkit';
+import authenticateReducer from "./authenticate/authenticate.slice";
+import darkModeReducer from "./darkmode/darkmode.slice";
+
+export const store = configureStore({
+    reducer: {
+        authenticate: authenticateReducer,
+        darkMode: darkModeReducer
+    }
+})
+
+// Lấy RootState và AppDispatch từ Store
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
