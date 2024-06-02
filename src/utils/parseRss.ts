feb71e5e9bf34238ac35ca9b37713390997a9e36
Le Anh Duc <ducvui2003@gmail.com>
2024-06-02 23:08:22 +0700

    refactor parse jsom from xml

--- src/utils/parseRss.ts
+++ src/utils/parseRss.ts
@@ -1,11 +1,22 @@
 import instance from "./request";
 import {Category} from "../constraints/category";
+import xmljs from 'xml-js';
 
-const parserRSS = async (category: Category ) => {
-    return await instance.get("/", {
+const parserRSS = async (category: Category) => {
+    return instance.get("/", {
         params: {
             "category": category
         }
-    });
+    }).then((xml) => {
+        const json = xmljs.xml2json(xml.data, {compact: true, spaces: 4});
+        console.log(JSON.parse(json))
+        return JSON.parse(json)
+    }).catch(error => {
+        console.log(error)
+        if (error.response.status == 500)
+            return;
+        if (erro.response.status == 400)
+            return;
+    })
 }
 export default parserRSS;
