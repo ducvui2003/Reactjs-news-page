eefd47fc19a653e2a22afdd0339f2ebfea3c3fe2
DucDai <phducdai47@gmail.com>
2024-06-03 17:54:08 +0700

    modify add className

--- src/components/Intro/CardItem/CardItem.tsx
+++ src/components/Intro/CardItem/CardItem.tsx
@@ -1,15 +1,18 @@
-import {Button, Card} from "react-bootstrap";
-import {News} from "../../../features/internationalNews/internationalNews";
+import { Button, Card } from "react-bootstrap";
+import { News } from "../../../features/internationalNews/internationalNews";
 
+interface CardItemProps extends News {
+    titleClassName?: string;
+    desClassName?: string;
+}
 
-
-export default function CardItem({imgLink, title, shortDes}:News) {
+export default function CardItem({ imgLink, title, titleClassName, shortDes, desClassName }: CardItemProps) {
     return (
-        <Card style={{width: '18rem'}}>
-            <Card.Img variant="top" src={imgLink}/>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={imgLink} />
             <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
+                <Card.Title className={titleClassName ? titleClassName.split(' ').join(' ') : ''}>{title}</Card.Title>
+                <Card.Text className={desClassName ? desClassName.split(' ').join(' ') : ''}>
                     {shortDes}
                 </Card.Text>
                 <Button variant="primary">Go somewhere</Button>
