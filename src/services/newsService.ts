e138d21c5f2c7755454782eb1b6f8077dc22df16
Lê Anh Đức <83511167+ducvui2003@users.noreply.github.com>
2024-06-04 13:38:34 +0700

    refactor xml to json  (#11)
    * add carousel for list news in category (limit 3 category and each category show 10 news)
    add react route for category click
    
    * Config proj from CRA -> Vite
    
    * feat: save info user to session storage, create service to check user
    
    * remove modal in redux, replace = state
    
    * handle text in footer
    
    * refactor parse jsom from xml
    
    * config git env product and dev, add npm run preview in package.json
    
    * ignore .env (variable local)

--- src/services/newsService.ts
+++ src/services/newsService.ts
@@ -4,21 +4,21 @@ import {News} from "../types/news.type";
 
 const getNewsByCategory = (category: Category): Promise<News[]> => {
     return parserRSS(category).then((res: any) => {
-        const rawNews = res.data.channel[0].item;
+        const rawNews = res.rss.channel.item;
         return convertToNew(rawNews);
     });
 }
 const convertToNew = (rawNews: any): News[] => {
     const domParser = new DOMParser();
     return rawNews.map((item: any) => {
-        const doc = domParser.parseFromString(item.description[0], "text/html");
+        const doc = domParser.parseFromString(item.description["_cdata"], "text/html");
         const thumbnail = doc.body.querySelector("img")?.getAttribute("src") || "";
         const description = doc.body.textContent?.trim() || '';
         return {
-            title: item.title[0],
-            link: item.link[0],
+            title: item.title["_cdata"],
+            link: item.link["_text"],
             description: description,
-            publishDate: new Date(item.pubDate[0]),
+            publishDate: new Date(item.pubDate["_text"]),
             thumbnail: thumbnail,
         }
     })
