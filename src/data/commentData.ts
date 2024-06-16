63ea28d6f8e1cf67bafb5f5293a7684a13c47603
DucDai <phducdai47@gmail.com>
2024-06-16 22:07:03 +0700

    add commentData.ts

---
+++ src/data/commentData.ts
@@ -0,0 +1,18 @@
+import { Comment } from '../types/comment.type';
+
+export const commentData: Comment[] = [
+  {
+    id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+    userId: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+    newsId: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+    content: 'Hello world',
+    createAt: new Date(),
+  },
+  {
+    id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6e',
+    userId: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6e',
+    newsId: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6e',
+    content: 'Hello world 2',
+    createAt: new Date(),
+  },
+];
