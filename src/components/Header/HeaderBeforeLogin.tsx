152741aaa99aed0e37519f6ef5709660c3ef63d4
Giang Nguyen <giangtruongnguyen02082003@gmail.com>
2024-06-25 15:58:34 +0700

    Save News (Login checking)

--- src/components/Header/HeaderBeforeLogin.tsx
+++ src/components/Header/HeaderBeforeLogin.tsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../features/store';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import ModalAuth from '../Dialog/ModalAuth';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
