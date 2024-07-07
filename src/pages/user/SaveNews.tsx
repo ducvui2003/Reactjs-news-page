fe2be636a5334fade4af8aa4393e3d5e3e479f6c
Trương Nguyễn Hương Giang <143255970+GiangNguyen0208@users.noreply.github.com>
2024-07-08 00:07:15 +0700

    fix import SaveNews.tsx

--- src/pages/user/SaveNews.tsx
+++ src/pages/user/SaveNews.tsx
@@ -1,3 +1,5 @@
+import { Grid } from '@mui/material';
+import NewsListSave from '../../components/NewsList/NewsListSave/NewsListSave';
 import React from 'react';
 
 const SaveNews: React.FC = () => {
