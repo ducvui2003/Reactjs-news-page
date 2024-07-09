04e19e6e3e788ddf2206fd27f515886ffba7614e
ducvui2003 <ducvui2003@gmail.com>
2024-07-09 17:30:03 +0700

    fix: scroll to top when click detail

--- src/components/SideBar/SideBarDetailLeft.tsx
+++ src/components/SideBar/SideBarDetailLeft.tsx
@@ -1,13 +1,13 @@
 import React, { RefObject, useEffect, useState } from 'react';
 import {
-  Box,
-  Grid,
-  List,
-  ListItem,
-  SpeedDialActionProps,
-  Theme,
-  Tooltip,
-  useMediaQuery,
+    Box,
+    Grid,
+    List,
+    ListItem,
+    SpeedDialActionProps,
+    Theme,
+    Tooltip,
+    useMediaQuery,
 } from '@mui/material';
 import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
@@ -24,152 +24,152 @@ import { toast } from 'react-toastify';
 import FloatButton from '../mobile/FloatButton';
 
 interface SideBarDetailLeftProps {
-  id: string;
-  targetComment: RefObject<HTMLElement>;
+    id: string;
+    targetComment: RefObject<HTMLElement>;
 }
 
 const SideBarDetailLeft = ({ id, targetComment }: SideBarDetailLeftProps) => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const savedNewsIds = useSelector((state: RootState) => state.news);
-  const user: User = useSelector((state: RootState) => state.authenticate); // Lấy trạng thái đăng nhập
-  const [isSaved, setIsSaved] = useState(false);
-  const isMobile = useMediaQuery((theme: Theme) =>
-    theme.breakpoints.down('sm'),
-  );
-
-  useEffect(() => {
-    setIsSaved(savedNewsIds.includes(id));
-  }, [savedNewsIds, id]);
-
-  const style = {
-    position: 'sticky',
-    left: 0,
-    top: '50px',
-    paddingTop: '50px',
-  };
-
-  const handleSaveNews = () => {
-    if (!user.email) {
-      toast.error('Bạn cần đăng nhập để lưu bài viết !');
-      return;
-    }
-    const getLastNum = 'A' + id.substring(id.length - 1, id.length);
-    if (isSaved) {
-      dispatch(unsave(getLastNum));
-      toast.error('Đã hủy lưu bài báo');
-    } else {
-      dispatch(save(getLastNum));
-      toast.success('Đã lưu bài báo');
-    }
-    setIsSaved(!isSaved);
-  };
-
-  const scrollToComment = () => {
-    targetComment.current?.scrollIntoView({ behavior: 'smooth' });
-  };
-
-  const actions: SpeedDialActionProps[] = [
-    {
-      icon: <ChatBubbleIcon sx={{ color: '#fff' }} />,
-      tooltipTitle: 'Bình luận',
-      onClick: () => {
-        scrollToComment();
-      },
-    },
-    {
-      icon: (
-        <BookmarkIcon
-          sx={{
-            backgroundColor: 'background.paper',
-            color: savedNewsIds.includes(id) ? '#0d6efd' : '#fff',
-          }}
-        />
-      ),
-      tooltipTitle: 'Lưu bài viết',
-      onClick: () => {
-        handleSaveNews();
-      },
-    },
-  ];
-  if (isMobile) {
-    return <FloatButton actions={actions} />;
-  } else {
-    return (
-      <Grid item md={1}>
-        <Box sx={style}>
-          <List
-            sx={{
-              display: 'flex',
-              flexDirection: { xs: 'row', md: 'column' },
-              overflowX: { xs: 'auto', md: 'visible' },
-            }}
-          >
-            <ListItem
-              sx={{
-                flex: { xs: '0 0 auto', md: '1 0 auto' },
-                width: { xs: 'auto', md: '100%' },
-              }}
-            >
-              <Tooltip
-                title={
-                  <Typography variant="body2" style={{ fontSize: '0.8rem' }}>
-                    Bình luận
-                  </Typography>
-                }
-                arrow
-                TransitionComponent={Zoom}
-              >
-                <IconButton
-                  onClick={() => scrollToComment()}
-                  sx={{ padding: '5px', backgroundColor: 'background.paper' }}
-                >
-                  <ChatBubbleIcon fontSize={'small'} />
-                </IconButton>
-              </Tooltip>
-            </ListItem>
-            <ListItem>
-              <Tooltip
-                title={
-                  <Typography variant="body2" style={{ fontSize: '0.8rem' }}>
-                    Lưu bài viết
-                  </Typography>
-                }
-                arrow
-              >
-                <IconButton
-                  onClick={() => handleSaveNews()}
-                  sx={{
-                    padding: '5px',
-                    backgroundColor: 'background.paper',
-                    color: savedNewsIds.includes(id) ? '#0d6efd' : '#757575',
-                  }}
-                >
-                  <BookmarkIcon fontSize={'small'} />
-                </IconButton>
-              </Tooltip>
-            </ListItem>
-            <ListItem>
-              <Tooltip
-                title={
-                  <Typography variant="body2" style={{ fontSize: '0.8rem' }}>
-                    Trở lại trang chủ
-                  </Typography>
-                }
-                arrow
-              >
-                <IconButton
-                  sx={{ padding: '5px', backgroundColor: 'background.paper' }}
-                  onClick={() => navigate('/')}
-                >
-                  <ArrowBackIcon fontSize={'small'} />
-                </IconButton>
-              </Tooltip>
-            </ListItem>
-          </List>
-        </Box>
-      </Grid>
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const savedNewsIds = useSelector((state: RootState) => state.news);
+    const user: User = useSelector((state: RootState) => state.authenticate); // Lấy trạng thái đăng nhập
+    const [isSaved, setIsSaved] = useState(false);
+    const isMobile = useMediaQuery((theme: Theme) =>
+        theme.breakpoints.down('sm'),
     );
-  }
+
+    useEffect(() => {
+        setIsSaved(savedNewsIds.includes(id));
+    }, [savedNewsIds, id]);
+
+    const style = {
+        position: 'sticky',
+        left: 0,
+        top: '50px',
+        paddingTop: '50px',
+    };
+
+    const handleSaveNews = () => {
+        if (!user.email) {
+            toast.error('Bạn cần đăng nhập để lưu bài viết !');
+            return;
+        }
+        const getLastNum = 'A' + id.substring(id.length - 1, id.length);
+        if (isSaved) {
+            dispatch(unsave(getLastNum));
+            toast.error('Đã hủy lưu bài báo');
+        } else {
+            dispatch(save(getLastNum));
+            toast.success('Đã lưu bài báo');
+        }
+        setIsSaved(!isSaved);
+    };
+
+    const scrollToComment = () => {
+        targetComment.current?.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    const actions: SpeedDialActionProps[] = [
+        {
+            icon: <ChatBubbleIcon />,
+            tooltipTitle: 'Bình luận',
+            onClick: () => {
+                scrollToComment();
+            },
+        },
+        {
+            icon: (
+                <BookmarkIcon
+                    sx={{
+                        backgroundColor: 'background.paper',
+                        color: savedNewsIds.includes(id) ? '#0d6efd' : '#757575',
+                    }}
+                />
+            ),
+            tooltipTitle: 'Lưu bài viết',
+            onClick: () => {
+                handleSaveNews();
+            },
+        },
+    ];
+    if (isMobile) {
+        return <FloatButton actions={actions} />;
+    } else {
+        return (
+            <Grid item md={1}>
+                <Box sx={style}>
+                    <List
+                        sx={{
+                            display: 'flex',
+                            flexDirection: { xs: 'row', md: 'column' },
+                            overflowX: { xs: 'auto', md: 'visible' },
+                        }}
+                    >
+                        <ListItem
+                            sx={{
+                                flex: { xs: '0 0 auto', md: '1 0 auto' },
+                                width: { xs: 'auto', md: '100%' },
+                            }}
+                        >
+                            <Tooltip
+                                title={
+                                    <Typography variant="body2" style={{ fontSize: '0.8rem' }}>
+                                        Bình luận
+                                    </Typography>
+                                }
+                                arrow
+                                TransitionComponent={Zoom}
+                            >
+                                <IconButton
+                                    onClick={() => scrollToComment()}
+                                    sx={{ padding: '5px', backgroundColor: 'background.paper' }}
+                                >
+                                    <ChatBubbleIcon fontSize={'small'} />
+                                </IconButton>
+                            </Tooltip>
+                        </ListItem>
+                        <ListItem>
+                            <Tooltip
+                                title={
+                                    <Typography variant="body2" style={{ fontSize: '0.8rem' }}>
+                                        Lưu bài viết
+                                    </Typography>
+                                }
+                                arrow
+                            >
+                                <IconButton
+                                    onClick={() => handleSaveNews()}
+                                    sx={{
+                                        padding: '5px',
+                                        backgroundColor: 'background.paper',
+                                        color: savedNewsIds.includes(id) ? '#0d6efd' : '#757575',
+                                    }}
+                                >
+                                    <BookmarkIcon fontSize={'small'} />
+                                </IconButton>
+                            </Tooltip>
+                        </ListItem>
+                        <ListItem>
+                            <Tooltip
+                                title={
+                                    <Typography variant="body2" style={{ fontSize: '0.8rem' }}>
+                                        Trở lại trang chủ
+                                    </Typography>
+                                }
+                                arrow
+                            >
+                                <IconButton
+                                    sx={{ padding: '5px', backgroundColor: 'background.paper' }}
+                                    onClick={() => navigate('/')}
+                                >
+                                    <ArrowBackIcon fontSize={'small'} />
+                                </IconButton>
+                            </Tooltip>
+                        </ListItem>
+                    </List>
+                </Box>
+            </Grid>
+        );
+    }
 };
 export default SideBarDetailLeft;
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,16 +1,16 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Navigate, NavLink, useParams } from 'react-router-dom';
 import details from '../data/newsDetail';
 import Comment from '../components/Comment/Comment';
 import {
-  Box,
-  Breadcrumbs,
-  CardMedia,
-  Container,
-  Grid,
-  Link,
-  Theme,
-  useMediaQuery,
+    Box,
+    Breadcrumbs,
+    CardMedia,
+    Container,
+    Grid,
+    Link,
+    Theme,
+    useMediaQuery,
 } from '@mui/material';
 import SideBarDetailLeft from '../components/SideBar/SideBarDetailLeft';
 import SideBarDetailRight from '../components/SideBar/SideBarDetailRight';
@@ -24,148 +24,152 @@ import { toCategoryName } from '../services/categoryService';
 import { formatDate } from '../utils/timeUtils';
 
 function Detail() {
-  const isMobile = useMediaQuery((theme: Theme) =>
-    theme.breakpoints.down('sm'),
-  );
-  const { id } = useParams<{ id: string }>();
-  if (!id) {
-    return <Navigate to={'/404'} />;
-  }
+    const isMobile = useMediaQuery((theme: Theme) =>
+        theme.breakpoints.down('sm'),
+    );
+    const { id } = useParams<{ id: string }>();
+    if (!id) {
+        return <Navigate to={'/404'} />;
+    }
 
-  const getLastNum = 'A' + id.substring(id.length - 1, id.length);
+    const getLastNum = 'A' + id.substring(id.length - 1, id.length);
 
-  const detail = details.find((item) => item.id == getLastNum);
+    const detail = details.find((item) => item.id == getLastNum);
 
-  if (!detail) {
-    return <Navigate to={'/404'} />;
-  }
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [id]);
 
-  const targetComment = useRef<HTMLElement>(null);
-  // Xử lý cho việc view ảnh
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const handleShowImage = () => {
-    setIsOpen(true);
-  };
+    if (!detail) {
+        return <Navigate to={'/404'} />;
+    }
 
-  const images = detail.paragraphs
-    .filter((para: Paragraph) => para.image?.link)
-    .map((para: Paragraph) => {
-      return {
-        src: para.image?.link,
-        alt: para.image?.capture,
-      };
-    });
+    const targetComment = useRef<HTMLElement>(null);
+    // Xử lý cho việc view ảnh
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const handleShowImage = () => {
+        setIsOpen(true);
+    };
 
-  return (
-    <Container>
-      <Grid
-        sx={{ position: isMobile ? 'relative' : 'static' }}
-        container
-        spacing={3}
-      >
-        <SideBarDetailLeft id={getLastNum} targetComment={targetComment} />
-        <Grid item md={7} xs={12}>
-          <Stack
-            sx={{ py: 3 }}
-            gap={isMobile ? 1 : 2}
-            direction={isMobile ? 'column' : 'row'}
-            justifyContent={'space-between'}
-            alignItems={isMobile ? 'flex-start' : 'center'}
-          >
-            <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />}>
-              <Link component={NavLink} to={'/'} underline="hover">
-                <Typography variant={'subtitle1'}> Trang chủ</Typography>
-              </Link>
-              <Link component={NavLink} to={'/'} underline="hover">
-                <Typography variant={'subtitle1'}>
-                  {toCategoryName(detail.category)}
-                </Typography>
-              </Link>
-            </Breadcrumbs>
-            <Typography variant={'subtitle1'}>
-              {formatDate(detail.publishDate)}
-            </Typography>
-          </Stack>
+    const images = detail.paragraphs
+        .filter((para: Paragraph) => para.image?.link)
+        .map((para: Paragraph) => {
+            return {
+                src: para.image?.link,
+                alt: para.image?.capture,
+            };
+        });
 
-          <Box sx={{ display: 'none' }}>
-            <SlideshowLightbox
-              images={images}
-              open={isOpen}
-              onClose={() => setIsOpen(false)}
-              fullScreen={true}
-              showThumbnails={false}
-              theme={'lightbox'}
-              animateThumbnails={true}
-            />
-          </Box>
-          <Typography gutterBottom component={'h1'} variant={'h2'}>
-            {detail.title}
-          </Typography>
-          <Typography gutterBottom align={'right'} variant={'subtitle1'}>
-            {`Tác giả ${detail.author}`}
-          </Typography>
-          {detail.paragraphs.map((para, index) => (
-            <NewParagraph
-              key={index}
-              paragraph={para}
-              showImage={handleShowImage}
-            />
-          ))}
-        </Grid>
-        <Grid item md={4} xs={12}>
-          <SideBarDetailRight category={detail.category} />
-        </Grid>
-      </Grid>
-      <Box ref={targetComment}>
-        <Comment newsId={getLastNum} />
-      </Box>
-    </Container>
-  );
+    return (
+        <Container>
+            <Grid
+                sx={{ position: isMobile ? 'relative' : 'static' }}
+                container
+                spacing={3}
+            >
+                <SideBarDetailLeft id={getLastNum} targetComment={targetComment} />
+                <Grid item md={7} xs={12}>
+                    <Stack
+                        sx={{ py: 3 }}
+                        gap={isMobile ? 1 : 2}
+                        direction={'row'}
+                        justifyContent={'space-between'}
+                        alignItems={isMobile ? 'flex-start' : 'center'}
+                    >
+                        <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />}>
+                            <Link component={NavLink} to={'/'} underline="hover">
+                                <Typography variant={'subtitle1'}> Trang chủ</Typography>
+                            </Link>
+                            <Link component={NavLink} to={'/'} underline="hover">
+                                <Typography variant={'subtitle1'}>
+                                    {toCategoryName(detail.category)}
+                                </Typography>
+                            </Link>
+                        </Breadcrumbs>
+                        <Typography variant={'subtitle1'}>
+                            {formatDate(detail.publishDate)}
+                        </Typography>
+                    </Stack>
+
+                    <Box sx={{ display: 'none' }}>
+                        <SlideshowLightbox
+                            images={images}
+                            open={isOpen}
+                            onClose={() => setIsOpen(false)}
+                            fullScreen={true}
+                            showThumbnails={false}
+                            theme={'lightbox'}
+                            animateThumbnails={true}
+                        />
+                    </Box>
+                    <Typography gutterBottom component={'h1'} variant={isMobile ? 'h3' : 'h2'}>
+                        {detail.title}
+                    </Typography>
+                    <Typography gutterBottom align={'right'} variant={'subtitle1'}>
+                        {`Tác giả ${detail.author}`}
+                    </Typography>
+                    {detail.paragraphs.map((para, index) => (
+                        <NewParagraph
+                            key={index}
+                            paragraph={para}
+                            showImage={handleShowImage}
+                        />
+                    ))}
+                </Grid>
+                <Grid item md={4} xs={12}>
+                    <SideBarDetailRight category={detail.category} />
+                </Grid>
+            </Grid>
+            <Box ref={targetComment}>
+                <Comment newsId={getLastNum} />
+            </Box>
+        </Container>
+    );
 }
 
 function NewsImage({
-  image,
-  showImage,
-}: {
-  image: Image | undefined;
-  showImage: () => void;
+                       image,
+                       showImage,
+                   }: {
+    image: Image | undefined;
+    showImage: () => void;
 }) {
-  if (typeof image == 'undefined') return <div></div>;
+    if (typeof image == 'undefined') return <div></div>;
 
-  return (
-    <Box>
-      <CardMedia component={'img'} image={image.link} onClick={showImage} />
-      <Typography
-        variant={'body2'}
-        gutterBottom
-        paragraph={true}
-        align={'center'}
-      >
-        {image.capture}
-      </Typography>
-    </Box>
-  );
+    return (
+        <Box>
+            <CardMedia component={'img'} image={image.link} onClick={showImage} />
+            <Typography
+                variant={'body2'}
+                gutterBottom
+                paragraph={true}
+                align={'center'}
+            >
+                {image.capture}
+            </Typography>
+        </Box>
+    );
 }
 
 function NewParagraph({
-  paragraph,
-  showImage,
-}: {
-  paragraph: Paragraph;
-  showImage: () => void;
+                          paragraph,
+                          showImage,
+                      }: {
+    paragraph: Paragraph;
+    showImage: () => void;
 }) {
-  return (
-    <>
-      <Typography gutterBottom variant={'body1'} align={'justify'}>
-        {paragraph.text}
-      </Typography>
-      <NewsImage
-        key={Math.random()}
-        image={paragraph.image}
-        showImage={showImage}
-      />
-    </>
-  );
+    return (
+        <>
+            <Typography gutterBottom variant={'body1'} align={'justify'}>
+                {paragraph.text}
+            </Typography>
+            <NewsImage
+                key={Math.random()}
+                image={paragraph.image}
+                showImage={showImage}
+            />
+        </>
+    );
 }
 
 export default Detail;
